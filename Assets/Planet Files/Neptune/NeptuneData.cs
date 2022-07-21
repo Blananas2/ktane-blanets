0a84a6740defc50d3491443ab7a06eab71106321
Blananas2 <blananas2@gmail.com>
2022-07-21 14:48:29 -0400

    ♆ Neptune
    Module by Blan, Help by BakersDozenBagels, tandyCake, Quinn Wuest

---
+++ Assets/Planet Files/Neptune/NeptuneData.cs
@@ -0,0 +1,207 @@
+﻿public class NeptuneData
+{
+    static public string[] Stars = {
+        "BOB 1    ", "BOB 2    ", "BOB 3    ", "BOB 4    ", "BOB 5    ", "BOB 6    ", "BOB  100C", 
+        "WF  1    ", "WF  2    ", "WF  3    ", "WF  4    ", "WF  5    ", "WF  6    ", "WF   100C", 
+        "JRB 1    ", "JRB 2    ", "JRB 3    ", "JRB 4    ", "JRB 5    ", "JRB 6    ", "JRB  100C", 
+        "CCM 1    ", "CCM 2    ", "CCM 3    ", "CCM 4    ", "CCM 5    ", "CCM 6    ", "CCM  100C", 
+        "BBH 1    ", "BBH 2    ", "BBH 3    ", "BBH 4    ", "BBH 5    ", "BBH 6    ", "BBH  100C", 
+        "HMC 1    ", "HMC 2    ", "HMC 3    ", "HMC 4    ", "HMC 5    ", "HMC 6    ", "HMC  100C", 
+        "LLL 1    ", "LLL 2    ", "LLL 3    ", "LLL 4    ", "LLL 5    ", "LLL 6    ", "LLL  100C", 
+        "SSL 1    ", "SSL 2    ", "SSL 3    ", "SSL 4    ", "SSL 5    ", "SSL 6    ", "SSL  100C", 
+        "DDD 1    ", "DDD 2    ", "DDD 3    ", "DDD 4    ", "DDD 5    ", "DDD 6    ", "DDD  100C", 
+        "SL  1    ", "SL  2    ", "SL  3    ", "SL  4    ", "SL  5    ", "SL  6    ", "SL   100C", 
+        "WDW 1    ", "WDW 2    ", "WDW 3    ", "WDW 4    ", "WDW 5    ", "WDW 6    ", "WDW  100C", 
+        "TTM 1    ", "TTM 2    ", "TTM 3    ", "TTM 4    ", "TTM 5    ", "TTM 6    ", "TTM  100C", 
+        "THI 1    ", "THI 2    ", "THI 3    ", "THI 4    ", "THI 5    ", "THI 6    ", "THI  100C", 
+        "TTC 1    ", "TTC 2    ", "TTC 3    ", "TTC 4    ", "TTC 5    ", "TTC 6    ", "TTC  100C", 
+        "RR  1    ", "RR  2    ", "RR  3    ", "RR  4    ", "RR  5    ", "RR  6    ", "RR   100C", 
+        "PSS     N", "PSS     F", "SA       ", "TOTWC    ", "COTMC    ", "VCUTM    ", "WMOTR    ",
+        "MIPS    1", "MIPS    2", "TOAD    1", "TOAD    2", "TOAD    3", "BITDW    ", "BITFS    ", "BITS     "
+    };
+
+    static public string[] LoggedStars = {
+        "BoB 1", "BoB 2", "BoB 3", "BoB 4", "BoB 5", "BoB 6", "BoB 100C", 
+        "WF 1", "WF 2", "WF 3", "WF 4", "WF 5", "WF 6", "WF 100C", 
+        "JRB 1", "JRB 2", "JRB 3", "JRB 4", "JRB 5", "JRB 6", "JRB 100C", 
+        "CCM 1", "CCM 2", "CCM 3", "CCM 4", "CCM 5", "CCM 6", "CCM 100C", 
+        "BBH 1", "BBH 2", "BBH 3", "BBH 4", "BBH 5", "BBH 6", "BBH 100C", 
+        "HMC 1", "HMC 2", "HMC 3", "HMC 4", "HMC 5", "HMC 6", "HMC 100C", 
+        "LLL 1", "LLL 2", "LLL 3", "LLL 4", "LLL 5", "LLL 6", "LLL 100C", 
+        "SSL 1", "SSL 2", "SSL 3", "SSL 4", "SSL 5", "SSL 6", "SSL 100C", 
+        "DDD 1", "DDD 2", "DDD 3", "DDD 4", "DDD 5", "DDD 6", "DDD 100C", 
+        "SL 1", "SL 2", "SL 3", "SL 4", "SL 5", "SL 6", "SL 100C", 
+        "WDW 1", "WDW 2", "WDW 3", "WDW 4", "WDW 5", "WDW 6", "WDW 100C", 
+        "TTM 1", "TTM 2", "TTM 3", "TTM 4", "TTM 5", "TTM 6", "TTM 100C", 
+        "THI 1", "THI 2", "THI 3", "THI 4", "THI 5", "THI 6", "THI 100C", 
+        "TTC 1", "TTC 2", "TTC 3", "TTC 4", "TTC 5", "TTC 6", "TTC 100C", 
+        "RR 1", "RR 2", "RR 3", "RR 4", "RR 5", "RR 6", "RR 100C", 
+        "PSS N", "PSS F", "SA", "TotWC", "CotMC", "VCutM", "WMotR",
+        "MIPS 1", "MIPS 2", "Toad 1", "Toad 2", "Toad 3", "BitDW", "BitFS", "BitS"
+    };
+
+    static public string[][] Enemies = new string[120][] {
+        new string[1]{"B+"},
+        new string[1]{"Kp+"},
+        new string[2]{"G", "B"},
+        new string[3]{"G", "B", "CC"},
+        new string[2]{"G", "B"},
+        new string[2]{"G", "B"},
+        new string[3]{"G", "Kp", "B"},
+        
+        new string[2]{"Wh", "Wh+"},
+        new string[4]{"G", "B", "PP", "Wh"},
+        new string[0]{}, //↖ Ø ←
+        new string[2]{"Wh", "Th"},
+        new string[2]{"Wh", "Wh+"},
+        new string[0]{}, //← Ø ↗
+        new string[2]{"PP", "Wh"},
+        
+        new string[0]{}, //↘ Ø ↗
+        new string[2]{"Cl", "Ee"},
+        new string[0]{}, //↙ N/A ↖
+        new string[1]{"Cl"},
+        new string[0]{}, //↘ Ø ↗
+        new string[2]{"Cl", "G"},
+        new string[2]{"Cl", "G"},
+        
+        new string[0]{}, //→ Ø ↘
+        new string[1]{"Pg-"},
+        new string[1]{"Pg+"},
+        new string[2]{"Sd", "MB"},
+        new string[2]{"Sn", "Sd"},
+        new string[1]{"Sd"},
+        new string[0]{}, //← Ø ↓
+        
+        new string[2]{"Bo", "Bo+"},
+        new string[4]{"S", "Bk", "Bo", "Bo+"},
+        new string[3]{"Bo", "Bo+", "Bk"},
+        new string[4]{"Pn", "Bk", "Bo", "Bo+"},
+        new string[4]{"S", "Bo", "Bo+", "Bk"},
+        new string[5]{"S", "Bo", "Bo+", "Bk", "MI"},
+        new string[5]{"MI", "Bo", "Bo+", "Pn", "Bk"},
+        
+        new string[2]{"MP", "S"},
+        new string[2]{"S", "Sw"},
+        new string[2]{"S", "Dr"},
+        new string[3]{"S", "Sf", "Sw"},
+        new string[3]{"S", "Sf", "Sw"},
+        new string[0]{}, //↘ N/A ↑
+        new string[4]{"S", "MI", "Sw", "Sf"},
+        
+        new string[1]{"Bl+"},
+        new string[2]{"Bl", "Bl+"},
+        new string[0]{}, //↓ Ø ↘
+        new string[0]{}, //↘ Ø ↓
+        new string[0]{}, //↓ Ø ↙
+        new string[0]{}, //↙ N/A ↘
+        new string[2]{"MI", "Bl"},
+        
+        new string[1]{"Kl"},
+        new string[0]{}, //← Ø →
+        new string[0]{}, //↙ N/A →
+        new string[5]{"F", "B", "Pk", "G", "Ey"},
+        new string[1]{"F"},
+        new string[0]{}, //↙ N/A →
+        new string[6]{"B", "F", "Pk", "G", "TB", "Gr"},
+
+        new string[0]{}, //↘ Ø ↓
+        new string[1]{"Cl"},
+        new string[0]{}, //↘ N/A ↗
+        new string[0]{}, //↙ Ø ↑
+        new string[1]{"MR"},
+        new string[0]{}, //↙ Ø ↗
+        new string[0]{}, //← N/A ↑
+
+        new string[2]{"Sd", "F"},
+        new string[1]{"Bl*"},
+        new string[1]{"Sd"},
+        new string[1]{"Sd"},
+        new string[1]{"Sd"},
+        new string[2]{"Sd", "G"},
+        new string[4]{"Sd", "MB", "Mn", "F"},
+
+        new string[0]{}, //↗ Ø ↓
+        new string[2]{"HH", "Cy"},
+        new string[2]{"HH", "Cy"},
+        new string[2]{"HH", "Cy"},
+        new string[1]{"HH"},
+        new string[1]{"HH"},
+        new string[3]{"Sk", "HH", "Cy"},
+
+        new string[5]{"Fw", "F", "MM", "B", "G"},
+        new string[1]{"Uk"},
+        new string[0]{}, //← Ø ↓
+        new string[0]{}, //↗ Ø ↙
+        new string[0]{}, //↑ Ø ↘
+        new string[0]{}, //↑ Ø ↓
+        new string[2]{"G", "Cy"},
+
+        new string[2]{"G-", "PP"},
+        new string[2]{"G-", "PP"},
+        new string[4]{"G-", "PP", "Kp+", "G+"},
+        new string[2]{"G-", "PP"},
+        new string[0]{}, //↑ N/A →
+        new string[4]{"G-", "PP", "Cy", "Wg"},
+        new string[2]{"G-", "PP"},
+
+        new string[4]{"HH", "Cy", "B", "FS"},
+        new string[3]{"F", "B", "FS"},
+        new string[0]{}, //↓ Ø ↘
+        new string[0]{}, //↙ N/A ↖
+        new string[3]{"F", "B", "FS"},
+        new string[0]{}, //→ Ø ↑
+        new string[1]{"B"},
+
+        new string[2]{"F", "Lk"},
+        new string[3]{"F", "Lk", "B"},
+        new string[2]{"F", "Lk"},
+        new string[4]{"Lk", "B", "F", "G"},
+        new string[3]{"Lk", "B", "F"},
+        new string[3]{"F", "Lk", "Cy"},
+        new string[3]{"F", "Lk", "B"},
+
+        new string[0]{}, //→ N/A →
+        new string[0]{}, //→ Ø →
+        new string[6]{"B", "G", "Bw", "F", "Pk", "FS"},
+        new string[0]{}, //→ Ø ←
+        new string[3]{"S", "Dr", "Sf"},
+        new string[0]{}, //← Ø ←
+        new string[0]{}, //← N/A ←
+
+        new string[1]{"MP"},
+        new string[1]{"MP"},
+        new string[0]{}, //← Ø ←
+        new string[0]{}, //← Ø ←
+        new string[0]{}, //← Ø ←
+
+        new string[2]{"G", "Bw"},
+        new string[0]{}, //← N/A →
+        new string[6]{"F", "Lk", "B", "FS", "G", "Bw"}
+    };
+
+    static public bool[] HasEnemies = {
+        true,  true,  true,  true,  true,  true,  true,  
+        true,  true,  false, true,  true,  false, true,  
+        false, true,  false, true,  false, true,  true,  
+        false, true,  true,  true,  true,  true,  false, 
+        true,  true,  true,  true,  true,  true,  true,  
+        true,  true,  true,  true,  true,  false, true,  
+        true,  true,  false, false, false, false, true,  
+        true,  false, false, true,  true,  false, true,  
+        false, true,  false, false, true,  false, false, 
+        true,  true,  true,  true,  true,  true,  true,  
+        false, true,  true,  true,  true,  true,  true,  
+        true,  true,  false, false, false, false, true,  
+        true,  true,  true,  true,  false, true,  true,  
+        true,  true,  false, false, true,  false, true,  
+        true,  true,  true,  true,  true,  true,  true,  
+        false, false, true,  false, true,  false, false,
+        true,  true,  false, false, false,  
+        true,  false, true    
+    };
+
+    static public int[] NoEnemies = { 9, 12, 14, 16, 18, 21, 27, 40, 44, 45, 46, 47, 50, 51, 54, 56, 58, 59, 61, 62, 70, 79, 80, 81, 82, 88,  93,  94, 96, 105, 106, 108, 110, 111, 114, 115, 116, 118};
+     static public int[] OddArrow = { 1, 11, 22, 22, 26, 22, 26, 48, 65, 53, 53, 53, 49, 57, 60, 64, 66, 65, 67, 60, 64, 78, 74, 74, 75, 74, 100, 100, 97, 107, 107, 109, 109, 109, 113, 113, 113, 117};
+    static public int[] EvenArrow = { 8,  6,  8,  8,  6, 29, 34, 33, 52, 52, 52, 55, 52, 52, 55, 63, 52, 62, 55, 55, 77, 86, 86, 89, 89, 89, 101,  86, 89, 107, 107, 107, 109, 109, 113, 113, 113, 119};
+}
---
+++ Assets/Planet Files/Neptune/NeptuneScript.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+
+public class NeptuneScript : MonoBehaviour { //depends on name
+
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+
+    public KMSelectable HideButton;
+    public GameObject Planet;
+    public GameObject StatStuff;
+    public KMSelectable[] PlanetButtons;
+    public GameObject Background;
+    public SpriteRenderer[] Sprites;
+    public Sprite[] Characters;
+    public Sprite Empty;
+    public GameObject[] Coins;
+    public GameObject Star;
+
+    bool Visible = true;
+    bool oddSN = false;
+    int starSpeed = 0;
+    int RNG = 0;
+    int chosenStar = 0;
+    int usedStar = 0;
+    int coinCount = 0;
+    int[] bannedRNGvalues = {-1, -1, -1, -1, -1, -1, -1};
+    string b36 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int a = 0; int b = 0; int c = 0; int d = 0; int e = 0;
+    int stage = 0;
+
+    //Logging
+    static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+    void Awake () {
+        moduleId = moduleIdCounter++;
+
+        foreach (KMSelectable PlanetButton in PlanetButtons) {
+            PlanetButton.OnInteract += delegate () { PlanetButtonPress(PlanetButton); return false; };
+        }
+
+        HideButton.OnInteract += delegate () { StartCoroutine(HidePlanet()); return false; };
+    }
+
+    // Use this for initialization
+    void Start () {
+        oddSN = Bomb.GetSerialNumberNumbers().Last() % 2 == 1;
+        starSpeed = UnityEngine.Random.Range(100, 201);
+        Debug.LogFormat("<Neptune #{0}> Star Speed: {1}", moduleId, starSpeed);
+        StartCoroutine(Rotation());
+        GeneratePuzzle();
+        ChangeRNGSprites();
+    }
+
+    void GeneratePuzzle() {
+        RNG = UnityEngine.Random.Range(0, 100000);
+        Debug.LogFormat("[Neptune #{0}] RNG starts at {1}.", moduleId, RNG);
+        bannedRNGvalues[0] = RNG;
+        RNGToLetters();
+
+        chosenStar = UnityEngine.Random.Range(0, 120);
+        Debug.LogFormat("[Neptune #{0}] Star is {1}.", moduleId, NeptuneData.LoggedStars[chosenStar]);
+
+        if (!NeptuneData.HasEnemies[chosenStar]) {
+            Debug.LogFormat("[Neptune #{0}] That star in the table has Ø or N/A.", moduleId);
+            int starIx = Array.IndexOf(NeptuneData.NoEnemies, chosenStar);
+            Debug.LogFormat("[Neptune #{0}] The serial number is {1}.", moduleId, (oddSN ? "odd" : "even"));
+            if (oddSN) {
+                usedStar = NeptuneData.OddArrow[starIx];
+            } else {
+                usedStar = NeptuneData.EvenArrow[starIx];
+            }
+            Debug.LogFormat("[Neptune #{0}] New star is {1}.", moduleId, NeptuneData.LoggedStars[usedStar]);
+        } else {
+            usedStar = chosenStar;
+        }
+
+        coinCount = NeptuneData.Enemies[usedStar].Count();
+        Debug.LogFormat("[Neptune #{0}] The number of coins shown is {1}.", moduleId, coinCount);
+
+        for (int n = 0; n < 6; n++) {
+            if (n >= coinCount) {
+                Coins[n].SetActive(false);
+            }
+        }
+
+        for (int s = 0; s < 9; s++) {
+            if (NeptuneData.Stars[chosenStar][s].ToString() == " ") {
+                Sprites[s+5].sprite = Empty;
+            } else {
+                Sprites[s+5].sprite = Characters[b36.IndexOf(NeptuneData.Stars[chosenStar][s])];
+            }
+        }
+
+        Debug.LogFormat("[Neptune #{0}] Sequence of enemies: {1}", moduleId, NeptuneData.Enemies[usedStar].Join(", "));
+    }
+
+    void ChangeRNGSprites () {
+        string RNGstring = RNG.ToString("00000");
+        for (int r = 0; r < 5; r++) {
+            Sprites[r].sprite = Characters[b36.IndexOf(RNGstring[r])];
+        }
+    }
+
+    void RNGToLetters () {
+        int R = RNG;
+        e = R % 10;
+        R = R / 10; d = R % 10;
+        R = R / 10; c = R % 10;
+        R = R / 10; b = R % 10;
+        R = R / 10; a = R % 10;
+    }
+
+    void LettersToRNG () {
+        RNG = a*10000 + b*1000 + c*100 + d*10 + e;
+    }
+
+    private IEnumerator Rotation() {
+        var elapsed = 0f;
+        while (true) {
+            Planet.transform.localEulerAngles = new Vector3(elapsed / 3954 * 360, 90f, 90f); //depends on time it takes to go around in 1 day
+            Star.transform.localEulerAngles = new Vector3(elapsed * starSpeed, 90f, 90f);
+            for (int c = 0; c < 6; c++) {
+                Coins[c].transform.localEulerAngles = new Vector3(elapsed * 144, 90f, 90f);
+            }
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+    }
+
+    private IEnumerator HidePlanet() {
+        for (int i = 0; i < 25; i++) {
+            yield return new WaitForSeconds(0.05f);
+            Background.transform.localScale += new Vector3(0f, 0.02f, 0f); //depends on size of the planet
+        }
+        Visible = !Visible;
+        if (!moduleSolved) {
+            Planet.SetActive(Visible);
+            StatStuff.SetActive(Visible);
+        } else {
+            Star.SetActive(Visible);
+        }
+        for (int i = 0; i < 25; i++) {
+            yield return new WaitForSeconds(0.05f);
+            Background.transform.localScale -= new Vector3(0f, 0.02f, 0f); //see above
+        }
+        Debug.LogFormat("<Neptune #{0}> Visible toggled to {1}.", moduleId, Visible);
+        yield return null;
+    }
+
+    void PlanetButtonPress(KMSelectable PlanetButton) {
+        if (moduleSolved) {
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            if (PlanetButtons[i] == PlanetButton) {
+                if (i == 0) {
+                    if (CheckEnemy(stage)) {
+                        Debug.LogFormat("[Neptune #{0}] {1} manipulated with {2}.", moduleId, NeptuneData.Enemies[usedStar][stage], RNG.ToString("00000"));
+                        stage += 1;
+                        Coins[coinCount - stage].SetActive(false);
+                        bool f = false;
+                        for (int k = 0; k < 7; k++) {
+                            if (bannedRNGvalues[k] == -1 && !f) {
+                                bannedRNGvalues[k] = RNG;
+                                f = true;
+                            }
+                        }
+                        if (coinCount == stage) {
+                            moduleSolved = true;
+                            Audio.PlaySoundAtTransform("star_collect", transform);
+                            Planet.SetActive(false);
+                            StatStuff.SetActive(false);
+                            StopAllCoroutines();
+                            StartCoroutine(AnimateStar());
+                        } else {
+                            PlanetButton.AddInteractionPunch();
+                            Audio.PlaySoundAtTransform("coin_collect", transform);
+                        }
+                    } else {
+                        Debug.LogFormat("[Neptune #{0}] {1} cannot be manipulated with {2}, Strike!", moduleId, NeptuneData.Enemies[usedStar][stage], RNG.ToString("00000"));
+                        GetComponent<KMBombModule>().HandleStrike();
+                    }
+                } else {
+                    Audio.PlaySoundAtTransform("so_" + ".BGRO"[i], transform);
+                    switch (i) {
+                        case 1:
+                            a = 9-a; b = 9-b; c = 9-c; d = 9-d; e = 9-e;
+                        break;
+                        case 2:
+                            b = (b+a)%10; c = (c+a)%10; d = (d+a)%10; e = (e+a)%10;
+                        break;
+                        case 3:
+                            b = (b*a)%10; c = (c*a)%10; d = (d*a)%10; e = (e*a)%10;
+                        break;
+                        case 4:
+                            int t = a; a = b; b = c; c = d; d = e; e = t;
+                        break;
+                    }
+                    LettersToRNG();
+                    ChangeRNGSprites();
+                }
+            }
+        }
+    }
+
+    bool CheckEnemy(int s) {
+        for (int m = 0; m < 7; m++) {
+            if (bannedRNGvalues[m] == RNG) {
+                return false;
+            }
+        }
+
+        string enemy = NeptuneData.Enemies[usedStar][s];
+        switch (enemy) {
+            case "B": return ((32768 < RNG) && (RNG < 65536));
+            case "F": return (a+b+c+d+e <= 20);
+            case "G": return (a+b+c+d+e >= 24);
+            case "S": return (a < b && b < c && c < d && d < e);
+            case "Sd": return (a > b && b > c && c > d && d > e);
+            case "Cy": return (a+b == d+e);
+            case "Lk": return (a+b+c+d == e);
+            case "PP": 
+                if (d == 0) {return false;} 
+                return ((a*100+b*10+c) % d == e);
+            case "Bo": return ((a*10+d) == (b*10+e));
+            case "Bo+": return ((a*100+b*10+c) == (c*100+d*10+e));
+            case "HH": return (a*b*c == (d*10+e));
+            case "Bk": return ((a*10+b) - (c*10+d) == e);
+            case "G-": return (a+b+c+d+e >= 12);
+            case "Cl": 
+                if ((c*10+d) == 0) {return false;} 
+                return ((float)(a*10+b) / (c*10+d) == (float)e);
+            case "FS": return ((b*100+e*10+d) == 222);
+            case "Wh": return (a*b > d*e);
+            case "MI": return ((c == 0) && (a*b*d*e != 0));
+            case "Sf": 
+                if (c == 0) {return false;} 
+                return (RNG % c == 6);
+            case "Sw": return ((a*10+b) == (c*10+d)*e);
+            case "Bw": return (a*b*c*d*e == 64);
+            case "MP": return (a+b-c+d-e < 0);
+            case "Pk": return ((b == a+1) && (c == a+2));
+            case "Bl": return ((b*100+c*10+d) < 256);
+            case "Bl+": return ((b*100+c*10+d) > 728);
+            case "Dr": return (a == (b*10+c) - (d*10+e));
+            case "Kp+": return (16384 > RNG);
+            case "MB": return ((a*10+b) + (c*10+d) == 100);
+            case "Pn": return (a*b*c*d*e != 0);
+            case "Wh+": return (a*b < d*e);
+            case "B+": return (32768 > RNG);
+            case "Bl*": return ((256 < (b*100+c*10+d)) && ((b*100+c*10+d) < 728));
+            case "CC": return ((e*100+c*10+a) % 111 == 0);
+            case "Ee": 
+                if ((c*10+d) == 0) {return false;} 
+                return ((a*10+b) % (c*10+d) == e);
+            case "Ey": return (a < b && b < c && c > d && d > e);
+            case "Fw": return (a > b && b > c && c < d && d < e);
+            case "G+": return (a+b+c+d+e >= 36);
+            case "Gr": return (RNG % 7 == 3);
+            case "Kl": return (a+b+c == (d*10+e));
+            case "Kp": return ((b*10+c) == 0);
+            case "MM": return (a-b+c-d+e < 0);
+            case "Mn": return (a*b*c*d*e > 255);
+            case "MR": return (a*e == b*d);
+            case "Pg-": return (1996 > RNG);
+            case "Pg+": return (98004 < RNG);
+            case "Sk": 
+                if ((c*10+b) == 0) {return false;} 
+                return ((float)(e*10+d) / (c*10+b) == (float)a);
+            case "Sn": 
+                if ((d*10+b) == 0) {return false;} 
+                return ((e*100+c*10+a) % (d*10+b) == 0);
+            case "TB": return (65536 < RNG);
+            case "Th": return ((c == e+2) && (d == e+1));
+            case "Uk": return (b == d && c == e && d != e);
+            case "Wg": return ((a*10+b) == (e*10+d));
+        }
+        return false;
+    }
+
+    IEnumerator AnimateStar () {
+        var elapsed = 0f;
+        Star.transform.localPosition = new Vector3(0f, 0.05f, -0.04f);
+        Star.transform.localScale = new Vector3(0.02f, 0.02f, -0.02f);
+        while (elapsed < 4.128f) {
+            Star.transform.localEulerAngles = new Vector3(elapsed * 394, 90f, 90f);
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+        Star.transform.localEulerAngles = new Vector3(180f, 90f, 90f);
+        GetComponent<KMBombModule>().HandlePass();
+        Debug.LogFormat("[Neptune #{0}] All enemies manipulated, module solved.", moduleId);
+    }
+}
