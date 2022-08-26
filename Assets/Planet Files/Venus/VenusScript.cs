33b732a7da31a858e19af5c292176694211ac4a6
tandyCake <dannypignuola@gmail.com>
2022-08-25 23:33:19 -0400

    Venus TP support & Autosolver

--- Assets/Planet Files/Venus/VenusScript.cs
+++ Assets/Planet Files/Venus/VenusScript.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using KModkit;
+using System.Text;
 
 public class VenusScript : MonoBehaviour { //depends on name
 
@@ -41,6 +42,7 @@ public class VenusScript : MonoBehaviour { //depends on name
                        "CGBMR", "CYGBR", "BRMCY", "RBGYC", "RMBGC", "RYMBC", 
                        "BGRMY", "MYCBG", "MRYCG", "MBRYG", "MCBRG", "BCGRY"};
     string backColor = "BGGCMBRBRRYCYMGMYGCRBCYM";
+    const string dirs = "ULRD";
     string[] directionNames = {"up", "left", "right", "down"};
 
     //Logging
@@ -69,10 +71,9 @@ public class VenusScript : MonoBehaviour { //depends on name
         int[] dummy = { 0, 1, -1, 2, 3 };
 
         start = UnityEngine.Random.Range(0, 24);
-        end = UnityEngine.Random.Range(0, 24);
-        while (start == end) {
-            end = UnityEngine.Random.Range(0, 24);
-        }
+        do end = UnityEngine.Random.Range(0, 24);
+        while (FindPath(start, end).Length < 2);
+
         Debug.Log("end = " + end);
         current = start;
 
@@ -116,13 +117,13 @@ public class VenusScript : MonoBehaviour { //depends on name
     private IEnumerator HidePlanet() {
         if (isAnimating) yield break;
         isAnimating = true;
-        yield return Ut.Animation(0.7f, d => Background.transform.localScale = new Vector3(1, Mathf.Lerp(1, 14, d), 1));
+        yield return Ut.Animation(0.75f, d => Background.transform.localScale = new Vector3(1, Mathf.Lerp(1, 14, d), 1));
         visible = !visible;
         Planet.SetActive(visible);
         for (int i = 0; i < 4; i++) {
             PBObjects[i].SetActive(visible);
         }
-        yield return Ut.Animation(0.7f, d => Background.transform.localScale = new Vector3(1, Mathf.Lerp(14, 1, d), 1));
+        yield return Ut.Animation(0.75f, d => Background.transform.localScale = new Vector3(1, Mathf.Lerp(14, 1, d), 1));
         Debug.LogFormat("<Venus #{0}> Visible toggled to {1}.", moduleId, visible);
         isAnimating = false;
     }
@@ -142,22 +143,12 @@ public class VenusScript : MonoBehaviour { //depends on name
                     string b = colors[current];
                     int[] xVals = new int[] { 1, 0, 0, -1 };
                     int[] zVals = new int[] { 0, 1, -1, 0 };
-                    string a = "";
-                    switch (i) {
-                        case 0: a = colors[current][2] + " " + colors[current][4]; break;
-                        case 1: a = colors[current][0] + " " + colors[current][3]; break;
-                        case 2: a = colors[current][0] + " " + colors[current][1]; break;
-                        case 3: a = backColor[current] + " " + colors[current][0]; break;
-                    }
-                    for (int j = 0; j < 24; j++) {
-                        if (a[0] == colors[j][0] && a[2] == colors[j][2]) {
-                            current = j;
-                            Debug.Log("current = " + current);
-                        }
-                    }
+                    current = MoveFrom(current, dirs[i]);
+                    Debug.Log("current = " + current);
                     Debug.LogFormat("[Venus #{0}] You went {1} from {2}, now you're at {3}.", moduleId, directionNames[i], b, colors[current]);
                     Audio.PlaySoundAtTransform("match", transform);
-                    StartCoroutine(GeXish(xVals[i], zVals[i]));
+                    //StartCoroutine(GeXish(xVals[i], zVals[i]));
+                    StartCoroutine(GeXishReborn(dirs[i]));
                 } else if (!ButtonIsValid(i)) {
                     GetComponent<KMBombModule>().HandleStrike();
                     Debug.LogFormat("[Venus #{0}] You tried to go {1} from {2} but you cannot go that way, strike!", moduleId, directionNames[i], colors[current]);
@@ -167,18 +158,22 @@ public class VenusScript : MonoBehaviour { //depends on name
     }
 
     bool ButtonIsValid(int i) {
-        return directions[current].Contains("ULRD"[i]);
+        return directions[current].Contains(dirs[i]);
     }
-
-    private IEnumerator GeXish(int xVal, int zVal)
+    private IEnumerator GeXishReborn(char dir)
     {
         Animating = true;
-        for (int i = 0; i < 30; i++)
+        Quaternion initRot = Planet.transform.localRotation;
+        Quaternion endRot = Quaternion.identity;
+        switch (dir)
         {
-            Planet.transform.RotateAround(Rotator.transform.position, Rotator.transform.right, 3f * xVal);
-            Planet.transform.RotateAround(Rotator.transform.position, Rotator.transform.forward, 3f * zVal);
-            yield return new WaitForSeconds(0.005f);
-        }
+            case 'U': endRot = Quaternion.AngleAxis( 90, Vector3.right  ) * initRot; break;
+            case 'L': endRot = Quaternion.AngleAxis( 90, Vector3.forward) * initRot; break;
+            case 'R': endRot = Quaternion.AngleAxis(-90, Vector3.forward) * initRot; break;
+            case 'D': endRot = Quaternion.AngleAxis(-90, Vector3.right  ) * initRot; break;
+        };
+        yield return Ut.Animation(0.25f, d => Planet.transform.localRotation = Quaternion.Slerp(initRot, endRot, d));
+
         Animating = false;
         CheckIfItsSolved();
     }
@@ -194,5 +189,108 @@ public class VenusScript : MonoBehaviour { //depends on name
             StartCoroutine(PlanetRotation());
         }
     }
+    string FindPath(int start, int end)
+    {
+        if (start == end) return "";
+        Queue<int> q = new Queue<int>();
+        List<Movement> allMoves = new List<Movement>();
+        q.Enqueue(start);
+        while (q.Count > 0)
+        {
+            int cur = q.Dequeue();
+            for (int d = 0; d < 4; d++)
+            {
+                if (directions[cur].Contains(dirs[d]))
+                {
+                    int newPos = MoveFrom(cur, dirs[d]);
+                    if (newPos != -1 && !allMoves.Any(x => x.start == newPos))
+                    {
+                        q.Enqueue(newPos);
+                        allMoves.Add(new Movement { start = cur, end = newPos, direction = dirs[d] });
+                    }
+                }
+            }
+            if (cur == end)
+            {
+                Debug.Log("Found end!");
+                break;
+            }
+        }
+        Debug.LogFormat("{0} -> {1}", start, end);
+        Movement lastMove = allMoves.First(x => x.end == end);
+        List<Movement> path = new List<Movement>() { lastMove };
+        while (lastMove.start != start)
+        {
+            lastMove = allMoves.First(x => x.end == lastMove.start);
+            path.Add(lastMove);
+        }
+        path.Reverse();
+        Debug.Log(path.Select(x => x.direction).Join(""));
+        return path.Select(x => x.direction).Join("");
+    }
+    int MoveFrom(int start, char d)
+    {
+        string cell = "";
+        string[] seq = (colors[start] + backColor[start]).Select(x => x.ToString()).ToArray();
+        switch (d)
+        {
+            case 'U': cell += seq[2] + seq[1] + seq[4] + seq[3] + seq[5]; break;
+            case 'L': cell += seq[0] + seq[2] + seq[3] + seq[5] + seq[4]; break;
+            case 'R': cell += seq[0] + seq[5] + seq[1] + seq[2] + seq[4]; break;
+            case 'D': cell += seq[5] + seq[1] + seq[0] + seq[3] + seq[2]; break;
+            default: throw new Exception("bitch");
+        }
+        return Array.IndexOf(colors, cell);
+    }
+    struct Movement
+    {
+        public int start;
+        public int end;
+        public char direction;
+    }
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use <!{0} start> to enter DARK VENUS! Use <!{0} move ULRD> to move up, left, right, then down. Use !{0} hide to press the hide button.";
+#pragma warning restore 414
 
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        command = command.Trim().ToUpperInvariant();
+        Match m = Regex.Match(command, @"^(?:MOVE\s+)?((?:[ULRD]\s*)+)$");
+        if (command == "HIDE")
+        {
+            yield return null;
+            HideButton.OnInteract();
+        }
+        else if (command == "START" && visible)
+        {
+            yield return null;
+            Debug.LogFormat("wero");
+            if (Started)
+                yield return "sendtochaterror DARK VENUS!!!!!!! is already entered :(";
+            else PlanetButtons[0].OnInteract();
+        }
+        else if (m.Success && Started && visible)
+        {
+            yield return null;
+            foreach (char dir in m.Groups[1].Value.Where(ch => "ULRD".Contains(ch)))
+            {
+                yield return new WaitUntil(() => !isAnimating);
+                yield return new WaitForSeconds(0.2f);
+                PlanetButtons[dirs.IndexOf(dir)].OnInteract();
+            }
+        }
+    }
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        if (!Started)
+            yield return Ut.Press(PlanetButtons[0], 0.2f);
+        foreach (char ch in FindPath(current, end))
+        {
+            yield return new WaitUntil(() => !isAnimating);
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log(ch);
+            PlanetButtons[dirs.IndexOf(ch)].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
