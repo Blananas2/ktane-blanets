db9ef4ef819f70459d8e59adf58088c165560482
Blananas2 <blananas2@gmail.com>
2023-07-23 11:38:59 -0400

    GitHub file migration

--- Assets/Planet Files/Neptune/NeptuneData.cs
+++ Assets/Planet Files/Neptune/NeptuneData.cs
@@ -1,4 +1,4 @@
-public class NeptuneData
+﻿public class NeptuneData
 {
     static public string[] Stars = {
         "BOB 1    ", "BOB 2    ", "BOB 3    ", "BOB 4    ", "BOB 5    ", "BOB 6    ", "BOB  100C", 
